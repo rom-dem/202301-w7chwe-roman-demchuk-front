6e477206f62d32dd8763c8cfa372ee597132599c
Róman Demchuk <roman.demchuk@gmail.com>
2023-02-26 17:14:18 +0100

    Add RegisterFormStyled

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.css";
+import GlobalStyles from "./GlobalStyles";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,6 +12,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
       <App />
     </Provider>
   </React.StrictMode>
